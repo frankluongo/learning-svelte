93569e5ff7dc58bd114a649e66e5ed201f66d786
frankluongo <frank@frankluongo.com>
2021-09-20 09:54:43 -0400

    SFB: templating

--- lut-svelte-for-beginners/levelup-quiz/src/Quiz.svelte
+++ lut-svelte-for-beginners/levelup-quiz/src/Quiz.svelte
@@ -1,17 +1,56 @@
 <script>
-  // It has to be `let` instead of `const`
-  export let name = 'Frank Quiz';
-  let input = ' ';
-  let a = 0;
-  let b = 0;
+  const answers = ['a', 'b', 'c', 'd'];
+  let result;
+  let correct = 'b';
+
+  function pickAnswer(answer) {
+    if (correct === answer) return (result = 'Correct!');
+    result = 'and I OOP';
+  }
 </script>
 
 <section>
-  <h2>My Quiz: {name}</h2>
-  <h3>{input}</h3>
-  <input bind:value={input} type="text" />
-  <hr />
-  <h3>{a + b}</h3>
-  <input bind:value={a} type="number" />
-  <input bind:value={b} type="number" />
+  <header>
+    {#if result}
+      <h3>{result}</h3>
+    {:else}
+      <h3>Pick an Answer</h3>
+    {/if}
+  </header>
+  <section class="buttons">
+    {#each answers as answer}
+      <button on:click={pickAnswer.bind(null, answer)}>
+        Answer {answer.toUpperCase()}
+      </button>
+    {/each}
+  </section>
 </section>
+
+<style>
+  .buttons {
+    display: flex;
+    justify-content: center;
+  }
+
+  .buttons > * + * {
+    margin-left: 1rem;
+  }
+
+  button {
+    padding: 0.5rem 1rem;
+
+    font-size: 0.875rem;
+
+    appearance: none;
+    background-color: #eee;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    transition: background-color 150ms ease-in-out;
+  }
+
+  button:focus,
+  button:hover {
+    background-color: #fff;
+  }
+</style>
